958bd7087ea66dfbaf4c6d16b8b73c415a86dce4
Bora Gönül <bora.gonul@mach.ist>
2023-09-06 15:11:08 +0300

    setup/lua

--- lua/machist/packer.lua
+++ lua/machist/packer.lua
@@ -45,6 +45,7 @@ return require('packer').startup(function(use)
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
   use("nvim-treesitter/nvim-treesitter-context");
+  use("fatih/vim-go");
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
